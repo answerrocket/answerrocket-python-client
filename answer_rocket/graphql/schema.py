92b393224f120caef0b84db43d2df6bb827f6440
Alexander Dodson <alexander.dodson@answerrocket.com>
2023-10-11 14:38:07 -0400

    wip on extracting the chat completion wrapper from artools into the external sdk. This does work if you set the key in the environment, but getting that key into the skill exec environment is trickier

--- answer_rocket/graphql/schema.py
+++ answer_rocket/graphql/schema.py
@@ -13,6 +13,10 @@ Boolean = sgqlc.types.Boolean
 
 DateTime = sgqlc.types.datetime.DateTime
 
+Float = sgqlc.types.Float
+
+Int = sgqlc.types.Int
+
 String = sgqlc.types.String
 
 class UUID(sgqlc.types.Scalar):
@@ -27,6 +31,16 @@ class UUID(sgqlc.types.Scalar):
 ########################################################################
 # Output Objects and Interfaces
 ########################################################################
+class LLMApiConfig(sgqlc.types.Interface):
+    __schema__ = schema
+    __field_names__ = ('id', 'api_type', 'model_type', 'model_name', 'is_active')
+    id = sgqlc.types.Field(sgqlc.types.non_null(UUID), graphql_name='id')
+    api_type = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='apiType')
+    model_type = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='modelType')
+    model_name = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='modelName')
+    is_active = sgqlc.types.Field(sgqlc.types.non_null(Boolean), graphql_name='isActive')
+
+
 class CopilotSkillArtifact(sgqlc.types.Type):
     __schema__ = schema
     __field_names__ = ('copilot_skill_artifact_id', 'copilot_id', 'copilot_skill_id', 'artifact_path', 'artifact', 'description', 'created_user_id', 'created_utc', 'last_modified_user_id', 'last_modified_utc', 'version', 'is_active', 'is_deleted')
@@ -47,7 +61,7 @@ class CopilotSkillArtifact(sgqlc.types.Type):
 
 class Query(sgqlc.types.Type):
     __schema__ = schema
-    __field_names__ = ('ping', 'get_copilot_skill_artifact_by_path')
+    __field_names__ = ('ping', 'get_copilot_skill_artifact_by_path', 'llmapi_config_for_sdk')
     ping = sgqlc.types.Field(String, graphql_name='ping')
     get_copilot_skill_artifact_by_path = sgqlc.types.Field(CopilotSkillArtifact, graphql_name='getCopilotSkillArtifactByPath', args=sgqlc.types.ArgDict((
         ('copilot_id', sgqlc.types.Arg(sgqlc.types.non_null(UUID), graphql_name='copilotId', default=None)),
@@ -55,6 +69,49 @@ class Query(sgqlc.types.Type):
         ('artifact_path', sgqlc.types.Arg(sgqlc.types.non_null(String), graphql_name='artifactPath', default=None)),
 ))
     )
+    llmapi_config_for_sdk = sgqlc.types.Field(LLMApiConfig, graphql_name='LLMApiConfigForSdk', args=sgqlc.types.ArgDict((
+        ('model_type', sgqlc.types.Arg(sgqlc.types.non_null(String), graphql_name='modelType', default=None)),
+))
+    )
+
+
+class AzureOpenaiCompletionLLMApiConfig(sgqlc.types.Type, LLMApiConfig):
+    __schema__ = schema
+    __field_names__ = ('api_base_url', 'api_version', 'openai_model_name', 'max_tokens_content_generation', 'temperature', 'top_p', 'presence_penalty', 'frequency_penalty', 'stop_sequence')
+    api_base_url = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='apiBaseUrl')
+    api_version = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='apiVersion')
+    openai_model_name = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='openaiModelName')
+    max_tokens_content_generation = sgqlc.types.Field(sgqlc.types.non_null(Int), graphql_name='maxTokensContentGeneration')
+    temperature = sgqlc.types.Field(sgqlc.types.non_null(Float), graphql_name='temperature')
+    top_p = sgqlc.types.Field(sgqlc.types.non_null(Float), graphql_name='topP')
+    presence_penalty = sgqlc.types.Field(sgqlc.types.non_null(Float), graphql_name='presencePenalty')
+    frequency_penalty = sgqlc.types.Field(sgqlc.types.non_null(Float), graphql_name='frequencyPenalty')
+    stop_sequence = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='stopSequence')
+
+
+class AzureOpenaiEmbeddingLLMApiConfig(sgqlc.types.Type, LLMApiConfig):
+    __schema__ = schema
+    __field_names__ = ('api_base_url', 'api_version')
+    api_base_url = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='apiBaseUrl')
+    api_version = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='apiVersion')
+
+
+class OpenaiCompletionLLMApiConfig(sgqlc.types.Type, LLMApiConfig):
+    __schema__ = schema
+    __field_names__ = ('organization', 'max_tokens_content_generation', 'temperature', 'top_p', 'presence_penalty', 'frequency_penalty', 'stop_sequence')
+    organization = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='organization')
+    max_tokens_content_generation = sgqlc.types.Field(sgqlc.types.non_null(Int), graphql_name='maxTokensContentGeneration')
+    temperature = sgqlc.types.Field(sgqlc.types.non_null(Float), graphql_name='temperature')
+    top_p = sgqlc.types.Field(sgqlc.types.non_null(Float), graphql_name='topP')
+    presence_penalty = sgqlc.types.Field(sgqlc.types.non_null(Float), graphql_name='presencePenalty')
+    frequency_penalty = sgqlc.types.Field(sgqlc.types.non_null(Float), graphql_name='frequencyPenalty')
+    stop_sequence = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='stopSequence')
+
+
+class OpenaiEmbeddingLLMApiConfig(sgqlc.types.Type, LLMApiConfig):
+    __schema__ = schema
+    __field_names__ = ('organization',)
+    organization = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='organization')
 
 
 
