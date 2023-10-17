08a8fc3fbf2cfda27406408c3b422011604387a7
Lawrence Parker <larry@answerrocket.com>
2023-10-17 16:52:55 -0400

    ExecuteSqlQueryResult

--- answer_rocket/data.py
+++ answer_rocket/data.py
@@ -4,11 +4,19 @@ from uuid import UUID
 import pandas as pd
 from typing import Optional
 
+from pandas import DataFrame
 from sgqlc.types import Variable, Arg, non_null, String, Int
 
 from answer_rocket.auth import AuthHelper
 from answer_rocket.graphql.client import GraphQlClient
 from answer_rocket.graphql.schema import UUID as GQL_UUID, ExecuteSqlQueryResponse
+
+
+class ExecuteSqlQueryResult:
+    success = False
+    code = None
+    error = None
+    df = None
 
 
 class Data:
@@ -20,7 +28,7 @@ class Data:
         self._auth_helper = auth_helper
         self._gql_client = gql_client
 
-    def execute_sql_query(self, database_id: UUID, sql_query: str, row_limit: Optional[int]) -> ExecuteSqlQueryResponse:
+    def execute_sql_query(self, database_id: UUID, sql_query: str, row_limit: Optional[int]) -> ExecuteSqlQueryResult:
         try:
             """
             sql_query: the SQL query to execute.
@@ -53,13 +61,36 @@ class Data:
 
             result = self._gql_client.submit(operation, query_args)
 
-            return result.execute_sql_query
+            execute_sql_query_response = result.execute_sql_query
+
+            execute_sql_query_result = ExecuteSqlQueryResult()
+
+            execute_sql_query_result.success = execute_sql_query_response.success
+            execute_sql_query_result.error = execute_sql_query_response.error
+            execute_sql_query_result.code = execute_sql_query_response.code
+
+            if execute_sql_query_response.success:
+                data = execute_sql_query_response.data
+
+                columns = [column["name"] for column in data["columns"]]
+                rows = [row["data"] for row in data["rows"]]
+
+                df = pd.DataFrame(rows, columns=columns)
+
+                execute_sql_query_result.df = df
+
+            return execute_sql_query_result
         except Exception as e:
-            execute_sql_query_response = ExecuteSqlQueryResponse(
-                json_data=None)
+            execute_sql_query_result = ExecuteSqlQueryResult()
 
-            execute_sql_query_response.success = False
-            execute_sql_query_response.error = e
-            execute_sql_query_response.code = 1000
+            execute_sql_query_result.success = False
+            execute_sql_query_result.error = e
+            execute_sql_query_result.code = 1000
 
-            return execute_sql_query_response
+            return execute_sql_query_result
+
+    def to_df(self, data) -> DataFrame:
+        columns = [column["name"] for column in data["columns"]]
+        rows = [row["data"] for row in data["rows"]]
+
+        df = pd.DataFrame(rows, columns=columns)
