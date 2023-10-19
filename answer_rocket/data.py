e6b230e7da64a680ff2da3af9a611fd643cf93d5
Lawrence Parker <larry@answerrocket.com>
2023-10-18 21:11:47 -0400

    Early work on datasets

--- answer_rocket/data.py
+++ answer_rocket/data.py
@@ -9,7 +9,7 @@ from sgqlc.types import Variable, Arg, non_null, String, Int
 
 from answer_rocket.auth import AuthHelper
 from answer_rocket.graphql.client import GraphQlClient
-from answer_rocket.graphql.schema import UUID as GQL_UUID, ExecuteSqlQueryResponse
+from answer_rocket.graphql.schema import UUID as GQL_UUID, ExecuteSqlQueryResponse, MaxMetricAttribute
 
 
 class ExecuteSqlQueryResult:
@@ -89,8 +89,99 @@ class Data:
 
             return execute_sql_query_result
 
-    def to_df(self, data) -> DataFrame:
-        columns = [column["name"] for column in data["columns"]]
-        rows = [row["data"] for row in data["rows"]]
+    def get_dataset_id(self, dataset_name: str) -> Optional[UUID]:
+        try:
+            """
+            dataset_name: the name of the dataset
+            """
+            query_args = {
+                'datasetName': dataset_name,
+            }
 
-        df = pd.DataFrame(rows, columns=columns)
+            query_vars = {
+                'dataset_name': Arg(non_null(String)),
+            }
+
+            operation = self._gql_client.query(variables=query_vars)
+
+            gql_query = operation.get_dataset_id(
+                dataset_name=Variable('dataset_name'),
+            )
+
+            result = self._gql_client.submit(operation, query_args)
+
+            gql_response = result.get_dataset_id
+
+            return gql_response
+        except Exception as e:
+            execute_sql_query_result = ExecuteSqlQueryResult()
+
+            execute_sql_query_result.success = False
+            execute_sql_query_result.error = e
+            execute_sql_query_result.code = 1000
+
+            return execute_sql_query_result
+
+    def get_metric_attribute_by_name(self, dataset_id: UUID, rql_name: str) -> MaxMetricAttribute:
+        try:
+            """
+            dataset_id: the UUID of the dataset
+            rql_name: the fully qualified RQL name of the metric (e.g. transactions.sales)
+            """
+            query_args = {
+                'datasetId': dataset_id,
+                'rqlName': rql_name
+            }
+
+            query_vars = {
+                'dataset_id': Arg(non_null(GQL_UUID)),
+                'rql_name': Arg(non_null(String)),
+            }
+
+            operation = self._gql_client.query(variables=query_vars)
+
+            gql_query = operation.get_metric_attribute_by_name(
+                dataset_id=Variable('dataset_id'),
+                rql_name=Variable('rql_name'),
+            )
+
+            gql_query.type()
+            gql_query.id()
+            gql_query.name()
+            gql_query.display_names()
+            gql_query.display_format()
+            gql_query.pluralized_display_name()
+            gql_query.hide_from_user()
+            gql_query.description()
+
+            gql_query.db_metric_column()
+            gql_query.agg_method()
+            gql_query.row_level_filter()
+            gql_query.positive_direction_up()
+            gql_query.can_be_averaged()
+            gql_query.not_additive()
+            gql_query.growth_output_format()
+            gql_query.hide_percentage_change()
+
+            gql_query.plural()
+            gql_query.headline_name()
+            gql_query.is_favorite()
+
+            gql_query.domain_entity()
+            gql_query.domain_entity().id()
+            gql_query.domain_entity().name()
+            gql_query.domain_entity().db_table()
+
+            result = self._gql_client.submit(operation, query_args)
+
+            gql_response = result.get_metric_attribute_by_name
+
+            return gql_response
+        except Exception as e:
+            execute_sql_query_result = ExecuteSqlQueryResult()
+
+            execute_sql_query_result.success = False
+            execute_sql_query_result.error = e
+            execute_sql_query_result.code = 1000
+
+            return execute_sql_query_result
--- answer_rocket/graphql/schema.py
+++ answer_rocket/graphql/schema.py
@@ -13,7 +13,14 @@ Boolean = sgqlc.types.Boolean
 
 DateTime = sgqlc.types.datetime.DateTime
 
+class DpsAggMethod(sgqlc.types.Enum):
+    __schema__ = schema
+    __choices__ = ('AVG', 'COUNT', 'CUSTOM', 'MAX', 'MIN', 'NONE', 'SUM')
+
+
 Float = sgqlc.types.Float
+
+ID = sgqlc.types.ID
 
 Int = sgqlc.types.Int
 
@@ -45,6 +52,48 @@ class LLMApiConfig(sgqlc.types.Interface):
     is_active = sgqlc.types.Field(sgqlc.types.non_null(Boolean), graphql_name='isActive')
 
 
+class MaxDomainObject(sgqlc.types.Interface):
+    __schema__ = schema
+    __field_names__ = ('type', 'id', 'name', 'description', 'display_names', 'pluralized_display_name')
+    type = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='type')
+    id = sgqlc.types.Field(sgqlc.types.non_null(ID), graphql_name='id')
+    name = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='name')
+    description = sgqlc.types.Field(String, graphql_name='description')
+    display_names = sgqlc.types.Field(sgqlc.types.non_null(sgqlc.types.list_of(sgqlc.types.non_null(String))), graphql_name='displayNames')
+    pluralized_display_name = sgqlc.types.Field(String, graphql_name='pluralizedDisplayName')
+
+
+class MaxDomainAttribute(sgqlc.types.Interface):
+    __schema__ = schema
+    __field_names__ = ('type', 'id', 'name', 'display_names', 'display_format', 'pluralized_display_name', 'hide_from_user', 'description', 'headline_name', 'is_favorite', 'domain_entity')
+    type = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='type')
+    id = sgqlc.types.Field(sgqlc.types.non_null(ID), graphql_name='id')
+    name = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='name')
+    display_names = sgqlc.types.Field(sgqlc.types.non_null(sgqlc.types.list_of(sgqlc.types.non_null(String))), graphql_name='displayNames')
+    display_format = sgqlc.types.Field(String, graphql_name='displayFormat')
+    pluralized_display_name = sgqlc.types.Field(String, graphql_name='pluralizedDisplayName')
+    hide_from_user = sgqlc.types.Field(sgqlc.types.non_null(Boolean), graphql_name='hideFromUser')
+    description = sgqlc.types.Field(String, graphql_name='description')
+    headline_name = sgqlc.types.Field(String, graphql_name='headlineName')
+    is_favorite = sgqlc.types.Field(Boolean, graphql_name='isFavorite')
+    domain_entity = sgqlc.types.Field('MaxDomainEntity', graphql_name='domainEntity')
+
+
+class MaxDomainEntity(sgqlc.types.Interface):
+    __schema__ = schema
+    __field_names__ = ('type', 'id', 'name', 'display_names', 'pluralized_display_name', 'db_table', 'db_columns', 'attributes', 'description', 'hide_from_user')
+    type = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='type')
+    id = sgqlc.types.Field(sgqlc.types.non_null(ID), graphql_name='id')
+    name = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='name')
+    display_names = sgqlc.types.Field(sgqlc.types.non_null(sgqlc.types.list_of(sgqlc.types.non_null(String))), graphql_name='displayNames')
+    pluralized_display_name = sgqlc.types.Field(String, graphql_name='pluralizedDisplayName')
+    db_table = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='dbTable')
+    db_columns = sgqlc.types.Field(sgqlc.types.non_null(sgqlc.types.list_of(String)), graphql_name='dbColumns')
+    attributes = sgqlc.types.Field(sgqlc.types.non_null(sgqlc.types.list_of(MaxDomainAttribute)), graphql_name='attributes')
+    description = sgqlc.types.Field(String, graphql_name='description')
+    hide_from_user = sgqlc.types.Field(Boolean, graphql_name='hideFromUser')
+
+
 class CopilotSkillArtifact(sgqlc.types.Type):
     __schema__ = schema
     __field_names__ = ('copilot_skill_artifact_id', 'copilot_id', 'copilot_skill_id', 'artifact_path', 'artifact', 'description', 'created_user_id', 'created_utc', 'last_modified_user_id', 'last_modified_utc', 'version', 'is_active', 'is_deleted')
@@ -74,7 +123,7 @@ class ExecuteSqlQueryResponse(sgqlc.types.Type):
 
 class Query(sgqlc.types.Type):
     __schema__ = schema
-    __field_names__ = ('ping', 'get_copilot_skill_artifact_by_path', 'execute_sql_query', 'llmapi_config_for_sdk')
+    __field_names__ = ('ping', 'get_copilot_skill_artifact_by_path', 'execute_sql_query', 'get_dataset_id', 'get_domain_entity', 'get_metric_attribute_by_name', 'get_domain_object_by_name', 'llmapi_config_for_sdk')
     ping = sgqlc.types.Field(String, graphql_name='ping')
     get_copilot_skill_artifact_by_path = sgqlc.types.Field(CopilotSkillArtifact, graphql_name='getCopilotSkillArtifactByPath', args=sgqlc.types.ArgDict((
         ('copilot_id', sgqlc.types.Arg(sgqlc.types.non_null(UUID), graphql_name='copilotId', default=None)),
@@ -86,6 +135,25 @@ class Query(sgqlc.types.Type):
         ('database_id', sgqlc.types.Arg(sgqlc.types.non_null(UUID), graphql_name='databaseId', default=None)),
         ('sql_query', sgqlc.types.Arg(sgqlc.types.non_null(String), graphql_name='sqlQuery', default=None)),
         ('row_limit', sgqlc.types.Arg(Int, graphql_name='rowLimit', default=None)),
+))
+    )
+    get_dataset_id = sgqlc.types.Field(UUID, graphql_name='getDatasetId', args=sgqlc.types.ArgDict((
+        ('dataset_name', sgqlc.types.Arg(sgqlc.types.non_null(String), graphql_name='datasetName', default=None)),
+))
+    )
+    get_domain_entity = sgqlc.types.Field(MaxDomainEntity, graphql_name='getDomainEntity', args=sgqlc.types.ArgDict((
+        ('dataset_id', sgqlc.types.Arg(sgqlc.types.non_null(UUID), graphql_name='datasetId', default=None)),
+        ('domain_entity_id', sgqlc.types.Arg(sgqlc.types.non_null(String), graphql_name='domainEntityId', default=None)),
+))
+    )
+    get_metric_attribute_by_name = sgqlc.types.Field('MaxMetricAttribute', graphql_name='getMetricAttributeByName', args=sgqlc.types.ArgDict((
+        ('dataset_id', sgqlc.types.Arg(sgqlc.types.non_null(UUID), graphql_name='datasetId', default=None)),
+        ('rql_name', sgqlc.types.Arg(sgqlc.types.non_null(String), graphql_name='rqlName', default=None)),
+))
+    )
+    get_domain_object_by_name = sgqlc.types.Field(UUID, graphql_name='getDomainObjectByName', args=sgqlc.types.ArgDict((
+        ('dataset_id', sgqlc.types.Arg(sgqlc.types.non_null(UUID), graphql_name='datasetId', default=None)),
+        ('domain_object_name', sgqlc.types.Arg(sgqlc.types.non_null(String), graphql_name='domainObjectName', default=None)),
 ))
     )
     llmapi_config_for_sdk = sgqlc.types.Field(LLMApiConfig, graphql_name='LLMApiConfigForSdk', args=sgqlc.types.ArgDict((
@@ -113,6 +181,80 @@ class AzureOpenaiEmbeddingLLMApiConfig(sgqlc.types.Type, LLMApiConfig):
     __field_names__ = ('api_base_url', 'api_version')
     api_base_url = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='apiBaseUrl')
     api_version = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='apiVersion')
+
+
+class MaxCalculatedMetric(sgqlc.types.Type, MaxDomainObject):
+    __schema__ = schema
+    __field_names__ = ('display_format', 'hide_from_user', 'rql', 'agg_method', 'positive_direction_up', 'can_be_averaged', 'not_additive', 'growth_output_format', 'hide_percentage_change', 'plural', 'headline_name')
+    display_format = sgqlc.types.Field(String, graphql_name='displayFormat')
+    hide_from_user = sgqlc.types.Field(sgqlc.types.non_null(Boolean), graphql_name='hideFromUser')
+    rql = sgqlc.types.Field(String, graphql_name='rql')
+    agg_method = sgqlc.types.Field(DpsAggMethod, graphql_name='aggMethod')
+    positive_direction_up = sgqlc.types.Field(Boolean, graphql_name='positiveDirectionUp')
+    can_be_averaged = sgqlc.types.Field(Boolean, graphql_name='canBeAveraged')
+    not_additive = sgqlc.types.Field(Boolean, graphql_name='notAdditive')
+    growth_output_format = sgqlc.types.Field(String, graphql_name='growthOutputFormat')
+    hide_percentage_change = sgqlc.types.Field(Boolean, graphql_name='hidePercentageChange')
+    plural = sgqlc.types.Field(Boolean, graphql_name='plural')
+    headline_name = sgqlc.types.Field(String, graphql_name='headlineName')
+
+
+class MaxDimensionEntity(sgqlc.types.Type, MaxDomainEntity, MaxDomainObject):
+    __schema__ = schema
+    __field_names__ = ('primary_attribute', 'archetype')
+    primary_attribute = sgqlc.types.Field('MaxPrimaryAttribute', graphql_name='primaryAttribute')
+    archetype = sgqlc.types.Field(String, graphql_name='archetype')
+
+
+class MaxFactEntity(sgqlc.types.Type, MaxDomainEntity, MaxDomainObject):
+    __schema__ = schema
+    __field_names__ = ()
+
+
+class MaxMetricAttribute(sgqlc.types.Type, MaxDomainAttribute, MaxDomainObject):
+    __schema__ = schema
+    __field_names__ = ('db_metric_column', 'agg_method', 'row_level_filter', 'positive_direction_up', 'can_be_averaged', 'not_additive', 'growth_output_format', 'hide_percentage_change', 'plural')
+    db_metric_column = sgqlc.types.Field(String, graphql_name='dbMetricColumn')
+    agg_method = sgqlc.types.Field(DpsAggMethod, graphql_name='aggMethod')
+    row_level_filter = sgqlc.types.Field(Boolean, graphql_name='rowLevelFilter')
+    positive_direction_up = sgqlc.types.Field(Boolean, graphql_name='positiveDirectionUp')
+    can_be_averaged = sgqlc.types.Field(Boolean, graphql_name='canBeAveraged')
+    not_additive = sgqlc.types.Field(Boolean, graphql_name='notAdditive')
+    growth_output_format = sgqlc.types.Field(String, graphql_name='growthOutputFormat')
+    hide_percentage_change = sgqlc.types.Field(Boolean, graphql_name='hidePercentageChange')
+    plural = sgqlc.types.Field(Boolean, graphql_name='plural')
+
+
+class MaxNormalAttribute(sgqlc.types.Type, MaxDomainAttribute, MaxDomainObject):
+    __schema__ = schema
+    __field_names__ = ('db_column', 'db_secondary_column', 'db_sort_column', 'is_sequence_attribute')
+    db_column = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='dbColumn')
+    db_secondary_column = sgqlc.types.Field(String, graphql_name='dbSecondaryColumn')
+    db_sort_column = sgqlc.types.Field(String, graphql_name='dbSortColumn')
+    is_sequence_attribute = sgqlc.types.Field(Boolean, graphql_name='isSequenceAttribute')
+
+
+class MaxPrimaryAttribute(sgqlc.types.Type, MaxDomainAttribute, MaxDomainObject):
+    __schema__ = schema
+    __field_names__ = ('load_dimension_values', 'db_primary_key_columns', 'db_secondary_column', 'left_outer_join_into', 'dimension_values', 'is_required_in_query', 'default_filter_value', 'db_sort_column', 'is_sequence_attribute', 'url_template')
+    load_dimension_values = sgqlc.types.Field(Boolean, graphql_name='loadDimensionValues')
+    db_primary_key_columns = sgqlc.types.Field(sgqlc.types.non_null(sgqlc.types.list_of(sgqlc.types.non_null(String))), graphql_name='dbPrimaryKeyColumns')
+    db_secondary_column = sgqlc.types.Field(String, graphql_name='dbSecondaryColumn')
+    left_outer_join_into = sgqlc.types.Field(Boolean, graphql_name='leftOuterJoinInto')
+    dimension_values = sgqlc.types.Field(sgqlc.types.list_of(String), graphql_name='dimensionValues')
+    is_required_in_query = sgqlc.types.Field(Boolean, graphql_name='isRequiredInQuery')
+    default_filter_value = sgqlc.types.Field(String, graphql_name='defaultFilterValue')
+    db_sort_column = sgqlc.types.Field(String, graphql_name='dbSortColumn')
+    is_sequence_attribute = sgqlc.types.Field(Boolean, graphql_name='isSequenceAttribute')
+    url_template = sgqlc.types.Field(String, graphql_name='urlTemplate')
+
+
+class MaxReferenceAttribute(sgqlc.types.Type, MaxDomainAttribute, MaxDomainObject):
+    __schema__ = schema
+    __field_names__ = ('db_foreign_key_columns', 'referenced_dimension_entity_id', 'left_outer_join')
+    db_foreign_key_columns = sgqlc.types.Field(sgqlc.types.non_null(sgqlc.types.list_of(String)), graphql_name='dbForeignKeyColumns')
+    referenced_dimension_entity_id = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='referencedDimensionEntityId')
+    left_outer_join = sgqlc.types.Field(sgqlc.types.non_null(Boolean), graphql_name='leftOuterJoin')
 
 
 class OpenaiCompletionLLMApiConfig(sgqlc.types.Type, LLMApiConfig):
