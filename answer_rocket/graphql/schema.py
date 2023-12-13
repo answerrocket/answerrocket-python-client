153c672b3baca347b17be5764609777090060ec0
Lawrence Parker <larry@answerrocket.com>
2023-12-13 12:28:45 -0500

    row_limit for execute_rql_query()

--- answer_rocket/graphql/schema.py
+++ answer_rocket/graphql/schema.py
@@ -223,6 +223,7 @@ class Query(sgqlc.types.Type):
     execute_rql_query = sgqlc.types.Field(ExecuteRqlQueryResponse, graphql_name='executeRqlQuery', args=sgqlc.types.ArgDict((
         ('dataset_id', sgqlc.types.Arg(sgqlc.types.non_null(UUID), graphql_name='datasetId', default=None)),
         ('rql_query', sgqlc.types.Arg(sgqlc.types.non_null(String), graphql_name='rqlQuery', default=None)),
+        ('row_limit', sgqlc.types.Arg(Int, graphql_name='rowLimit', default=None)),
 ))
     )
     get_dataset_id = sgqlc.types.Field(UUID, graphql_name='getDatasetId', args=sgqlc.types.ArgDict((
