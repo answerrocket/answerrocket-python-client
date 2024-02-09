29335dc79305753d6abaa246ee5dea8b0db7d518
Lawrence Parker <larry@answerrocket.com>
2024-02-09 11:40:27 -0500

    MaxMetricHierarchyNode

--- answer_rocket/graphql/schema.py
+++ answer_rocket/graphql/schema.py
@@ -272,7 +272,7 @@ class MaxDatabase(sgqlc.types.Type):
 
 class MaxDataset(sgqlc.types.Type):
     __schema__ = schema
-    __field_names__ = ('dataset_id', 'name', 'domain_objects', 'misc_info', 'database', 'tables', 'dimension_value_distribution_map', 'date_range_boundary_attribute_id', 'dimension_hierarchies', 'domain_attribute_statistics')
+    __field_names__ = ('dataset_id', 'name', 'domain_objects', 'misc_info', 'database', 'tables', 'dimension_value_distribution_map', 'date_range_boundary_attribute_id', 'dimension_hierarchies', 'metric_hierarchies', 'domain_attribute_statistics')
     dataset_id = sgqlc.types.Field(sgqlc.types.non_null(UUID), graphql_name='datasetId')
     name = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='name')
     domain_objects = sgqlc.types.Field(sgqlc.types.non_null(sgqlc.types.list_of(sgqlc.types.non_null(MaxDomainObject))), graphql_name='domainObjects')
@@ -282,16 +282,16 @@ class MaxDataset(sgqlc.types.Type):
     dimension_value_distribution_map = sgqlc.types.Field(JSON, graphql_name='dimensionValueDistributionMap')
     date_range_boundary_attribute_id = sgqlc.types.Field(String, graphql_name='dateRangeBoundaryAttributeId')
     dimension_hierarchies = sgqlc.types.Field(sgqlc.types.non_null(sgqlc.types.list_of('MaxDimensionHierarchyNode')), graphql_name='dimensionHierarchies')
+    metric_hierarchies = sgqlc.types.Field(sgqlc.types.non_null(sgqlc.types.list_of('MaxMetricHierarchyNode')), graphql_name='metricHierarchies')
     domain_attribute_statistics = sgqlc.types.Field(JSON, graphql_name='domainAttributeStatistics')
 
 
 class MaxDimensionHierarchyNode(sgqlc.types.Type):
     __schema__ = schema
-    __field_names__ = ('dimension_hierarchy_node_id', 'dimension_attribute_id', 'description', 'exclude_from_market_size', 'children')
+    __field_names__ = ('dimension_hierarchy_node_id', 'dimension_attribute_id', 'description', 'children')
     dimension_hierarchy_node_id = sgqlc.types.Field(sgqlc.types.non_null(UUID), graphql_name='dimensionHierarchyNodeId')
     dimension_attribute_id = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='dimensionAttributeId')
     description = sgqlc.types.Field(String, graphql_name='description')
-    exclude_from_market_size = sgqlc.types.Field(sgqlc.types.non_null(Boolean), graphql_name='excludeFromMarketSize')
     children = sgqlc.types.Field(sgqlc.types.non_null(sgqlc.types.list_of(sgqlc.types.non_null('MaxDimensionHierarchyNode'))), graphql_name='children')
 
 
@@ -302,6 +302,15 @@ class MaxDomainAttributeStatisticInfo(sgqlc.types.Type):
     min_value = sgqlc.types.Field(String, graphql_name='minValue')
     max_value = sgqlc.types.Field(String, graphql_name='maxValue')
     distinct_count = sgqlc.types.Field(Int, graphql_name='distinctCount')
+
+
+class MaxMetricHierarchyNode(sgqlc.types.Type):
+    __schema__ = schema
+    __field_names__ = ('metric_hierarchy_node_id', 'metric_id', 'description', 'children')
+    metric_hierarchy_node_id = sgqlc.types.Field(sgqlc.types.non_null(UUID), graphql_name='metricHierarchyNodeId')
+    metric_id = sgqlc.types.Field(sgqlc.types.non_null(String), graphql_name='metricId')
+    description = sgqlc.types.Field(String, graphql_name='description')
+    children = sgqlc.types.Field(sgqlc.types.non_null(sgqlc.types.list_of(sgqlc.types.non_null('MaxMetricHierarchyNode'))), graphql_name='children')
 
 
 class MaxMutationResponse(sgqlc.types.Type):
