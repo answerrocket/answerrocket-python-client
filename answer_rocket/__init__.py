87b32d0d65f78c44a643b1ec1cc613da34dc9a84
Alexander Dodson <alexander.dodson@answerrocket.com>
2024-05-09 11:19:58 -0400

    bump version to 0.2.1 for tagging

--- answer_rocket/__init__.py
+++ answer_rocket/__init__.py
@@ -3,7 +3,7 @@ __all__ = [
     'AnswerRocketClient'
 ]
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 from answer_rocket.client import AnswerRocketClient
 from answer_rocket.error import AnswerRocketClientError
