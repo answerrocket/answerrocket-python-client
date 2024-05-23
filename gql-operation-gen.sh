88f946c23b610158088e2fcf7f8c8bf7fb1328fc
Alexander Dodson <alexander.dodson@answerrocket.com>
2024-05-23 09:59:02 -0400

    workaround for sgqlc-codegen not quite handling multiple operation input files properly

--- gql-operation-gen.sh
+++ gql-operation-gen.sh
@@ -6,7 +6,11 @@ python -m sgqlc.introspection \
 	"http://localhost:8080/api/sdk/graphql" \
 	schema.json
 
+# sgqlc-codegen accepts multiple input files but it doesn't quite work, if we don't merge them then we get
+# redundant definitions of the top level Query/Mutation/Operation classes that overwrite each other.
+cat answer_rocket/graphql/operations/[!_]*.gql > answer_rocket/graphql/operations/_merged.gql
+
 sgqlc-codegen operation --schema schema.json \
   .schema \
   answer_rocket/graphql/sdk_operations.py \
-  answer_rocket/graphql/operations/*.gql
+  answer_rocket/graphql/operations/_merged.gql
