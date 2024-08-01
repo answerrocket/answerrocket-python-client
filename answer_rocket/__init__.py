9df1d48174950a451d34c000948c11bd99f1f43c
Lawrence Parker <larry@answerrocket.com>
2024-08-01 17:04:54 -0400

    version bump

--- answer_rocket/__init__.py
+++ answer_rocket/__init__.py
@@ -3,7 +3,7 @@ __all__ = [
     'AnswerRocketClient'
 ]
 
-__version__ = "0.2.11"
+__version__ = "0.2.12"
 
 from answer_rocket.client import AnswerRocketClient
 from answer_rocket.error import AnswerRocketClientError
