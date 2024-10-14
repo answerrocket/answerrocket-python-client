609d1f9a1090139cffd2680dbcc8edc824b4c2b3
Grant Barchard <grant.barchard@answerrocket.com>
2024-10-14 15:59:25 -0400

    version bump

--- answer_rocket/__init__.py
+++ answer_rocket/__init__.py
@@ -3,7 +3,7 @@ __all__ = [
     'AnswerRocketClient'
 ]
 
-__version__ = "0.2.18"
+__version__ = "0.2.19"
 
 from answer_rocket.client import AnswerRocketClient
 from answer_rocket.error import AnswerRocketClientError
