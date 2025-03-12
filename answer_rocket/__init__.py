4ffff4fe353d7e309a9d06d59b94e88240058c1e
Lawrence Parker <larry@answerrocket.com>
2025-03-12 11:14:36 -0400

    version

--- answer_rocket/__init__.py
+++ answer_rocket/__init__.py
@@ -3,7 +3,7 @@ __all__ = [
     'AnswerRocketClient'
 ]
 
-__version__ = "0.2.36"
+__version__ = "0.2.37"
 
 from answer_rocket.client import AnswerRocketClient
 from answer_rocket.error import AnswerRocketClientError
