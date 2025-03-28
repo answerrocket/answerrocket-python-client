0d0a4f41bd1c1e83b342a1453869e36821e3fe0c
AnswerRocketMike <mike@answerrocket.com>
2025-03-28 11:47:25 -0400

    version bump

--- answer_rocket/__init__.py
+++ answer_rocket/__init__.py
@@ -3,7 +3,7 @@ __all__ = [
     'AnswerRocketClient'
 ]
 
-__version__ = "0.2.40"
+__version__ = "0.2.41"
 
 from answer_rocket.client import AnswerRocketClient
 from answer_rocket.error import AnswerRocketClientError
