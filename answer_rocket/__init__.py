96f1b5a89e2e8cbb8cf7fdd0b3bf4107f5fe6bcd
Preston Goren <preston.goren@answerrocket.com>
2025-04-04 10:44:32 -0400

    version bump

--- answer_rocket/__init__.py
+++ answer_rocket/__init__.py
@@ -3,7 +3,7 @@ __all__ = [
     'AnswerRocketClient'
 ]
 
-__version__ = "0.2.41"
+__version__ = "0.2.42"
 
 from answer_rocket.client import AnswerRocketClient
 from answer_rocket.error import AnswerRocketClientError
