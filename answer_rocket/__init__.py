e1ab7f31807e55942b93392873182312db3ff7ac
Crawford <crawford.nakayama@answerrocket.com>
2025-07-17 16:49:06 -0500

    bump version

--- answer_rocket/__init__.py
+++ answer_rocket/__init__.py
@@ -5,7 +5,7 @@ __all__ = [
     'MetaDataFrame'
 ]
 
-__version__ = "0.2.61"
+__version__ = "0.2.62"
 
 from answer_rocket.client import AnswerRocketClient
 from answer_rocket.error import AnswerRocketClientError
