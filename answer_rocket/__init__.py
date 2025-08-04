c8fa9819661c9bd1a16917413e8be7e179eee390
Kevin Wang <kevin.wang@answerrocket.com>
2025-08-04 09:46:57 -0400

    bump version

--- answer_rocket/__init__.py
+++ answer_rocket/__init__.py
@@ -5,7 +5,7 @@ __all__ = [
     'MetaDataFrame'
 ]
 
-__version__ = "0.2.68"
+__version__ = "0.2.69"
 
 from answer_rocket.client import AnswerRocketClient
 from answer_rocket.error import AnswerRocketClientError
