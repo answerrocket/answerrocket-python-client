d0a05771d37f5e047a1084cb1716a786fc632f21
Lawrence Parker <larry@answerrocket.com>
2025-09-30 15:03:41 -0400

    version

--- answer_rocket/__init__.py
+++ answer_rocket/__init__.py
@@ -5,7 +5,7 @@ __all__ = [
     'MetaDataFrame'
 ]
 
-__version__ = "0.2.86"
+__version__ = "0.2.87"
 
 from answer_rocket.client import AnswerRocketClient
 from answer_rocket.error import AnswerRocketClientError
