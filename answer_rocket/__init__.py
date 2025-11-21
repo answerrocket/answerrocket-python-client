bae6fca44e574f025941a5bda97331b41744faee
RedTailedHawk <RedTailedHawk@users.noreply.github.com>
2025-11-21 10:13:43 -0500

    version

--- answer_rocket/__init__.py
+++ answer_rocket/__init__.py
@@ -5,7 +5,7 @@ __all__ = [
     'MetaDataFrame'
 ]
 
-__version__ = "0.2.96"
+__version__ = "0.2.97"
 
 from answer_rocket.client import AnswerRocketClient
 from answer_rocket.error import AnswerRocketClientError
