7737a55f6353b3a5dcc21b79a7adad71de6ca4f5
Jonathan Leo <jonathan.leo@answerrocket.com>
2026-05-15 16:41:39 -0400

    version bump

--- answer_rocket/__init__.py
+++ answer_rocket/__init__.py
@@ -5,7 +5,7 @@ __all__ = [
     'MetaDataFrame'
 ]
 
-__version__ = "0.2.104"
+__version__ = "0.2.105"
 
 from answer_rocket.client import AnswerRocketClient
 from answer_rocket.error import AnswerRocketClientError
