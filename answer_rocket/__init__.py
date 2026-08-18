1d62375b41b640b026042eda83806d6f15924b8b
Chris Liding <chris.liding@answerrocket.com>
2026-08-18 12:47:05 -0400

    Version bump

--- answer_rocket/__init__.py
+++ answer_rocket/__init__.py
@@ -5,7 +5,7 @@ __all__ = [
     'MetaDataFrame'
 ]
 
-__version__ = "0.2.107"
+__version__ = "0.2.108"
 
 from answer_rocket.client import AnswerRocketClient
 from answer_rocket.error import AnswerRocketClientError
